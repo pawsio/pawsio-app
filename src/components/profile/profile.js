69d92216d5fa4fad7a48557bd30174e050d2be84
Nathan Hugon <nthugon@gmail.com>
2016-12-30 12:39:36 -0800

    routes to profile from welcome defaulting to profile.pets

---
+++ src/components/profile/profile.js
@@ -0,0 +1,11 @@
+import template from './profile.html';
+import styles from './profile.scss';
+
+export default {
+    template,
+    controller
+};
+
+function controller() {
+    this.styles = styles;
+}
--- src/routes.js
+++ src/routes.js
@@ -11,6 +11,37 @@ export default function routes($stateProvider, $urlRouterProvider) {
         component: 'welcome' 
     });
 
+    $stateProvider.state({
+        name: 'profile',
+        url: '/profile',
+        abstract: true,
+        default: '.pets',
+        data: {
+            public: true
+        },
+        // resolve: {
+        //     pets: ['yetToBeMadeService', pets => {
+        //         return pets.get();
+        //     }]
+        // },
+        component: 'profile'
+    });
+
+    $stateProvider.state({
+        name: 'profile.pets',
+        url: '/pets',
+        data: {
+            public: true
+        },
+        component: 'allPets'
+    });
+
+    $stateProvider.state({
+        name: 'profile.manage',
+        url: '/manage',
+        component: 'managePets'
+    });
+
     $urlRouterProvider.otherwise('/welcome');
     
 }
