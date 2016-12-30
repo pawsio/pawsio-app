27bdd694c07ddd7ac96742ef16bbd2a5be1562a2
Nathan Hugon <nthugon@gmail.com>
2016-12-30 10:40:41 -0800

    top level config files setup

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const cssExtract = new ExtractTextPlugin('main.css');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './build',
+        filename: 'main.js'
+    },
+    devtool: 'source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new ExtractTextPlugin('main.css')
+    ],
+    module: {
+        preLoaders: [{
+            test: /\.js$/,
+            loader: 'eslint-loader',
+            exclude: /node_modules/
+        }],
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader:'babel-loader',
+            query: {
+                cacheDirectory: true,   
+            } 
+        }, {
+            test: /\.css$/,
+            loader: cssExtract.extract(
+                'style-loader',
+                'css-loader'
+            )	
+        }, {
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css?sourceMap!sass?sourceMap')
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader'
+        }]
+    },
+    sassLoader: {
+        includePaths: ['./src/scss/partials']
+    }
+};
+
+
