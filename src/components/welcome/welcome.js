0b8c08048d21b0625d423aaa44a66ec19975a196
Nathan Hugon <nthugon@gmail.com>
2016-12-30 11:25:04 -0800

    welcome component set up/webpack builds successfully

---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,11 @@
+import template from './welcome.html';
+import styles from './welcome.scss';
+
+export default {
+    template,
+    controller
+};
+
+function controller() {
+    this.styles = styles;
+}
