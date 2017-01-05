3fca0a63fd2b1533757d6384aec38874652fe93a
Nathan Hugon <nthugon@gmail.com>
2017-01-05 09:42:29 -0800

    sample chart setup

--- src/components/snapshot/snapshot.js
+++ src/components/snapshot/snapshot.js
@@ -1,5 +1,6 @@
 import template from './snapshot.html';
 import styles from './snapshot.scss';
+import Chart from 'chart.js';
 
 export default {
     template, 
@@ -12,11 +13,31 @@ export default {
 controller.$inject = ['kineticsService', 'petSnapshotService', '$state'];
 
 function controller(kineticsService, petSnapshotService, $state) {
+
+    // let ctx = "bar_chart_canvas";
+    // let wkly_summary_chart = new Chart(ctx, { // eslint-disable-line no-unused-vars, no-undef
+    //     type: 'bar',
+    //     data: {
+    //         labels: 'Some Measurement',
+    //         datasets: [{
+    //             label: 'Actual Hours',
+    //             data: '120 minutes',
+    //             backgroundColor: '#382765'
+    //         },
+    //         {
+    //             label: 'Target Hours',
+    //             data: '90 minutes',
+    //             backgroundColor: '#7BC225'
+    //         }]
+    //     }
+    // });
+
     this.styles = styles;
     this.velArr = [];
     this.distArr = [];
 
     this.$onInit = function () {
+        
         kineticsService.getVelocity(this.snapshot.dataPayload)
         .then(velArr => {
             let rawHikeLength = velArr[(velArr.length) - 1].timeStamp;
@@ -44,4 +65,42 @@ function controller(kineticsService, petSnapshotService, $state) {
             $state.go('stats');
         });
     };
+
+    var ctx = "myChart";
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
 }
